db07fbbdfb0eafeda66b27d973376ae9378064e3
redsiamesecat <an919757449@vip.qq.com>
2020-07-06 22:56:42 +0800

    初始化项目+底部菜单栏

---
+++ src/components/FooterGuide/FooterGuide.vue
@@ -0,0 +1,72 @@
+<template>
+    <footer class="footer_guide ">
+      <a href="javacsript:;" class="guide_item" :class="{on:'/home' === $route.path}" @click="goTo('/home')">
+        <span class="item_icon">
+          <i class="iconfont icon-waimai"></i>
+        </span>
+        <span>外卖</span>
+      </a>
+      <a href="javacsript:;" class="guide_item" :class="{on:'/search' === $route.path}" @click="goTo('/search')">
+        <span class="item_icon">
+          <i class="iconfont icon-search"></i>
+        </span>
+        <span>搜索</span>
+      </a>
+      <a href="javacsript:;" class="guide_item" :class="{on:'/order' === $route.path}" @click="goTo('/order')">
+        <span class="item_icon">
+          <i class="iconfont icon-dingdan"></i>
+        </span>
+        <span>订单</span>
+      </a>
+      <a href="javacsript:;" class="guide_item" :class="{on:'/profile' === $route.path}" @click="goTo('/profile')">
+        <span class="item_icon">
+          <i class="iconfont icon-geren"></i>
+        </span>
+        <span>我的</span>
+      </a>
+    </footer>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  methods: {
+    // 点击跳转对应组件的事件
+    goTo (path) {
+      this.$router.replace(path)
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+  .footer_guide
+    position fixed
+    z-index 100
+    left 0
+    right 0
+    bottom 0
+    background-color #fff
+    width 100%
+    height 50px
+    display flex
+    .guide_item
+      display flex
+      text-decoration none
+      flex-direction column
+      align-items center
+      flex 1
+      margin 5px
+      text-align center
+      color #999999
+      &.on
+        color #02a774
+        span
+          font-size 12px
+          margin-top 2px
+          margin-bottom 2px
+          .iconfont
+            font-size 22px
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,38 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/Home/Home.vue'
+import Order from '../views/Order/Order.vue'
+import Profile from '../views/Profile/Profile.vue'
+import Search from '../views/Search/Search.vue'
+
+// 设置避免因为路由重复引起报错
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push (location) {
+  return originalPush.call(this, location).catch(err => err)
+}
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: '/Home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/order',
+    component: Order
+  },
+  {
+    path: '/profile',
+    component: Profile
+  },
+  {
+    path: '/serch',
+    component: Search
   }
 ]
 
